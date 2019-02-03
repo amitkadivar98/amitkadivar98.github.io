bad63de09a83e541447a1b39e177a54d3ba36773
amitkadivar1 <amit.kadivar1@gmail.com>
2019-02-03 09:38:15 +0530

    add

---
+++ build/precache-manifest.81bdb940269fd55dade33f79b050b666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f976abb4fea56f396248",
+    "url": "/static/js/main.f976abb4.chunk.js"
+  },
+  {
+    "revision": "4f6f38307e0186354f47",
+    "url": "/static/js/1.4f6f3830.chunk.js"
+  },
+  {
+    "revision": "f976abb4fea56f396248",
+    "url": "/static/css/main.a7282114.chunk.css"
+  },
+  {
+    "revision": "d69be7698bc00a0b204408eda06441e4",
+    "url": "/index.html"
+  }
+];
